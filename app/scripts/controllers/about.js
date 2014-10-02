5fae5c0eb880c554301864537ae663deda249fca
Daniel Schmidt <dschmidt@weluse.de>
2014-10-02 22:08:14 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsChoreoBuilderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jsChoreoBuilderApp
+ */
+angular.module('jsChoreoBuilderApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsChoreoBuilderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jsChoreoBuilderApp
+ */
+angular.module('jsChoreoBuilderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
