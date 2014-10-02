2b7b90d1f368335344c1dd35c31db8f1cc661788
Daniel Schmidt <dschmidt@weluse.de>
2014-10-02 23:03:47 +0200

    fixed tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,10 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should be able to add a move', function () {
+    var oldLength = scope.availableMoves.length;
+
+    scope.addMove('Test');
+    expect(scope.availableMoves.length).toBe(oldLength + 1);
   });
 });
