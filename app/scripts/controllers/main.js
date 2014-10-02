b5e3bd55851c8f35dfc3016ab718d0f08f321406
Daniel Schmidt <dschmidt@weluse.de>
2014-10-02 23:41:27 +0200

    made it feature complete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,47 @@
  */
 angular.module('jsChoreoBuilderApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
+    var sortableEle;
+
+    // available moves should be persisted
     $scope.availableMoves = (localStorageService.get('availableMoves') || []);
     localStorageService.bind($scope, 'availableMoves', $scope.availableMoves);
+
+    // chosen moves should be persisted
+    $scope.chosenMoves = (localStorageService.get('chosenMoves') || []);
+    localStorageService.bind($scope, 'chosenMoves', $scope.chosenMoves);
 
     $scope.addMove = function() {
       $scope.availableMoves.push($scope.newMove);
       $scope.newMove = '';
     };
 
+    $scope.addChosenMove = function(newMove) {
+      $scope.chosenMoves.push(newMove);
+    };
+
     $scope.removeMove = function(move) {
       $scope.availableMoves.splice($scope.availableMoves.indexOf(move), 1);
     };
+
+    $scope.removeChosenMove = function(move) {
+      $scope.chosenMoves.splice($scope.chosenMoves.indexOf(move), 1);
+    };
+
+    $scope.dragStart = function(e, ui) {
+      ui.item.data('start', ui.item.index());
+    }
+
+    $scope.dragEnd = function(e, ui) {
+      var start = ui.item.data('start'),
+          end = ui.item.index();
+
+      $scope.chosenMoves.splice(end, 0, $scope.chosenMoves.splice(start, 1)[0]);
+      $scope.$apply();
+    }
+
+    sortableEle = $('.js-sortable').sortable({
+        start: $scope.dragStart,
+        update: $scope.dragEnd
+    });
   });
